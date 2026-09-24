2e2635318bbd0fc1c5a5f6b73005e4565a6f3dca
JadonLee8 <jadonlee888@gmail.com>
2026-09-24 13:34:34 -0500

    access env values from env so you don't have to export

--- dbSetup.java
+++ dbSetup.java
@@ -1,3 +1,6 @@
+import java.io.FileInputStream;
+import java.util.Properties;
+
 /*
 CSCE 331
 Database setup example
@@ -5,7 +8,19 @@ Purpose: Keeping usernames and password private.
 */
 public final class dbSetup  {
   public static final String user = "squad_44";
-  public static final String pswd = System.getenv("db_pass");
+  public static final String pswd = loadPassword();
   public static final String dtbs = "squad_44_db";
-  
+
+  // Reads db_pass from a local .env file, falling back to a real
+  // environment variable if the file or key isn't present.
+  private static String loadPassword() {
+    try (FileInputStream in = new FileInputStream(".env")) {
+      Properties props = new Properties();
+      props.load(in);
+      return props.getProperty("db_pass", System.getenv("db_pass"));
+    } catch (Exception e) {
+      return System.getenv("db_pass");
+    }
+  }
+
 }//end class
