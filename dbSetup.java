505cf335058c701920ce04316698911a65fe15de
DunsinK <133840905+DunsinK@users.noreply.github.com>
2026-09-24 12:58:36 -0500

    initial commit -password

---
+++ dbSetup.java
@@ -0,0 +1,11 @@
+/*
+CSCE 331
+Database setup example
+Purpose: Keeping usernames and password private.
+*/
+public final class dbSetup  {
+  public static final String user = "squad_44";
+  public static final String pswd = System.getenv("db_pass");
+  public static final String dtbs = "squad_44_db";
+  
+}//end class
